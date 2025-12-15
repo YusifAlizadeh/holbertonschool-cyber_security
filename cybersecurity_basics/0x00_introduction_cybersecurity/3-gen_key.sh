825bf8c662f4dc31a2b08af40d9c8299c8d2dc08
YusifAlizadeh <alizade.yusif111@gmail.com>
2025-12-15 08:11:42 +0000

    Bash script that generates an RSA SSH key pair

--- cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-ssh-keygen -t rsa -b 4096 -f "$1"
+if [ -z "$1" ]; then
+  echo "Usage: $0 <key_name>"
+  exit 1
+fi
+
+KEY_NAME="$1"
+
+# Generate RSA 4096-bit SSH key using OpenSSH
+ssh-keygen -t rsa -b 4096 -f "$KEY_NAME"
