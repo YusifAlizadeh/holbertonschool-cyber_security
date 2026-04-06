affaf52391be1c321a162eef343cbeec6b77f261
YusifAlizadeh <alizade.yusif111@gmail.com>
2026-04-06 09:58:27 +0000

    a Bash script to identify the IP address responsible for the most requests in a log file, which is likely the source of a Denial of Service (DoS) attack

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Define log file
 LOG_FILE="logs.txt"
 
-# Check if file exists
 if [[ ! -f "$LOG_FILE" ]]; then
-    echo "File $LOG_FILE not found."
     exit 1
 fi
 
-# Extract IPs, count them, sort by most frequent, and display the top result
-awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 1
+# Extract IP, count, sort, take the top one, then print ONLY the IP column
+awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
