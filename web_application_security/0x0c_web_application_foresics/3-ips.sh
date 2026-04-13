7d738c8391ddf49d9436fa27499d0b24fd80885a
YusifAlizadeh <alizade.yusif111@gmail.com>
2026-04-13 10:21:09 +0000

    add scripts for account and IP forensic analysis

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "Accepted password for root" auth.log | grep -Eo '[0-9]{1,3}(.[0-9]{1,3}){3}' | sort -u | wc -l
+grep "Accepted" auth.log | grep -v "root" | awk '{print $(NF-3)}' | sort -u | wc -l
